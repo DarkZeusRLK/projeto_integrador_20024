6a8c50ff3420c5a7929a019eff8465677ae733ee
Dieimes Nunes <114628166+DieimesNunes@users.noreply.github.com>
2024-08-30 11:04:36 -0300

    ffg

--- index.php
+++ index.php
@@ -14,12 +14,15 @@ $retorno_consulta = $mysqli->query($consultar_banco) or die($mysqli->error);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/stylee.css">
+
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script defer src="javascript/script_navbar.js"></script>
     <title>Início - IvaíTour</title>
+    <!--
     <style>
         body {
             background-color: #fff;
@@ -68,6 +71,8 @@ $retorno_consulta = $mysqli->query($consultar_banco) or die($mysqli->error);
             background-color: #0056b3;
         }
     </style>
+
+-->
 </head>
 
 <body>
