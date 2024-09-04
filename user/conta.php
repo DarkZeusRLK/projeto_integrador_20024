7d610d111a82a075d1008f59c55229921019953e
ZERE029 <matheusvital029@gmail.com>
2024-09-04 20:14:46 -0300

    att

---
+++ user/conta.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="img/logo2.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="icon" href="Imagens/icon.png">
+    <link rel="stylesheet" href="../css/zere.css">
+    <title>Minha Conta</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <style>
+        body, html {
+            font-family: Arial, sans-serif;
+            height: 100%;
+            margin: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .divo {
+            width: 900px; /* ajuste conforme necessário */
+            padding: 20px;
+       }
+       /* zere.css */
+
+/* Estilos gerais */
+
+/* Efeito de hover para o botão com id 'but1' */
+/* Ajuste de layout para dispositivos móveis */
+@media (max-width: 768px) {
+    .divo {
+        width: 100%;
+        padding: 30px;
+    }
+
+    .profile-picture-container {
+        display: flex;
+        justify-content: center;
+    }
+
+    .profile-picture {
+        width: 150px;
+        height: 150px;
+        object-fit: cover;
+        border-radius: 50%;
+    }
+
+    .button-group {
+        flex-direction: column;
+        align-items: center;
+        gap: 5px;
+    }
+
+    .form-control {
+        width: 100%;
+    }
+
+    main {
+        padding: 10px;
+    }
+
+    .info-title {
+        display: inline-block;
+        font-weight: bold;
+    }
+}
+
+/* Ajustes para telas maiores */
+@media (min-width: 769px) {
+    .profile-picture {
+        width: 200px;
+        height: 200px;
+    }
+
+    .button-group {
+        flex-direction: row;
+    }
+
+    main {
+        padding: 20px;
+    }
+}
+    </style>
+</head>
+
+<body>
+    <div class="divo">
+        <div class="container profile-container">
+            <div class="text-center mb-4">
+                <div class="profile-picture-container">
+                        <img class='profile-picture' src='../Imagens/avatar2.png' alt='Foto de perfil'>
+                </div>
+                <h1 class="mt-3">Usuário</h1>
+            </div>
+
+            <form action="upload_imagem.php" method="post" enctype="multipart/form-data" class="mb-4" id="uploadForm">
+                <input type="file" name="foto" id="foto" class="form-control" placeholder="Mudar foto de perfil" required>
+                <div class="button-group">
+                    <a id="but1" href="editaconta.php" class="btn btn-primary">Alterar suas Informações</a>
+                </div>
+            </form>
+            <h2 class="mb-3">Minhas informações:</h2>
+            <main class="escuro">
+                <h4><span class="info-title">Nome:</span></h4>
+                <h4><span class="info-title">Endereço:</span></h4>
+                <h4><span class="info-title">Email:</span></h4>
+                <h4><span class="info-title">CPF:</span></h4>
+                <div class="account-section">
+                    </div>
+                </main>
+                <h2>Minhas viagens</h2>
+                <main class="escuro"></main>
+        </div>
+    </div>
+
+</body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</html>
---
+++ user/metodo_pag.php
@@ -0,0 +1,144 @@
+<?php
+if (empty($opcaoSelecionada))
+    echo "Nenhuma opção selecionada.";
+if (isset($_POST['metodo'])) {
+    $metodo = $_POST['metodo'];
+
+    switch ($metodo) {
+        case 'cartao':
+            header("Location: cartao.php");
+            break;
+        case 'pix':
+            header("Location: pix.php");
+            break;
+        case 'boleto':
+            header("Location: boleto.php");
+            break;
+        default:
+            header("Location: metodo_pag.php");
+            break;
+    }
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Escolha Seu Método de Pagamento</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/zere.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        .payment-option {
+            margin-bottom: 10px;
+        }
+
+        .payment-option input {
+            margin-right: 10px;
+        }
+
+        .submit-button {
+            margin-top: 20px;
+        }
+
+        .divo {
+        width: 100%;
+        padding: 30px;
+    }
+
+    .profile-picture-container {
+        display: flex;
+        justify-content: center;
+    }
+
+    .profile-picture {
+        width: 150px;
+        height: 150px;
+        object-fit: cover;
+        border-radius: 50%;
+    }
+
+    .button-group {
+        flex-direction: column;
+        align-items: center;
+        gap: 5px;
+    }
+
+    .form-control {
+        width: 100%;
+    }
+
+    main {
+        padding: 10px;
+    }
+
+    .info-title {
+        display: inline-block;
+        font-weight: bold;
+    }
+
+
+/* Ajustes para telas maiores */
+@media (min-width: 769px) {
+    .profile-picture {
+        width: 200px;
+        height: 200px;
+    }
+
+    .button-group {
+        flex-direction: row;
+    }
+
+    main {
+        padding: 20px;
+    }
+}
+    </style>
+</head>
+
+<body>
+    <h1>Escolha Seu Método de Pagamento</h1>
+    <div class="divo">
+        <form action="metodo_pag.php" method="post">
+            <div class="dive">
+                <div class="payment-option">
+                    <label>
+                        <input type="radio" name="metodo" value="cartao" required>
+                        <img src="../Imagens/credit-card.png" alt="" width="35px">
+                        Cartão de Crédito
+                    </label>
+                </div>
+            </div>
+            <div class="dive">
+            <div class="payment-option">
+                <label>
+                    <input type="radio" name="metodo" value="pix" required>
+                    <img src="../imagens/iconepix.png" alt="" width="25px">
+                    Pix
+                </label>
+            </div>
+            </div>
+    <div class="dive">
+    <div class="payment-option">
+                <label>
+                    <input type="radio" name="metodo" value="boleto" required>
+                    <img src="../imagens/boleto.png" alt="" height="20px">
+                    Transferência Bancária
+                </label>
+            </div>
+    </div>
+            <button type="submit" id="but1" class="btn btn-primary">Prosseguir</button>
+        </form>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</html>
