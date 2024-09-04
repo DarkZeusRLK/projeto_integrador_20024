634dd797560dd76991dd2a2ba6ad011c3dbba4ea
JOAO RAFAEL DE CARVALHO BISSOLOTI <bissolotijoaorafael@hotmail.com>
2024-09-04 12:00:03 -0300

    Atualização

--- contato.php
+++ contato.php
@@ -9,19 +9,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/ctt.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="../#Imagens/Icon.png">
     <title>Contato</title>
 </head>
 <body>
 
-    <?php
-    include('static/menu.php');
-    ?>
+<div class="container-fluid">
+        <nav class="col-md-3 col-lg-2 sidebar">
+            <div class="menu-btn" onclick="toggleSidebar()">&#9776;</div>
+            <div class="profile">
+                <img id="logo" src="Imagens/logo (1).png" alt="Logo">
+                <h1 class="text-title">IvaíTour</h1>
+            </div>
+            <ul class="nav-links">
+                <li><a href="#home"><i class="fas fa-home"></i><span>Home</span></a></li>
+                <li><a href="#services"><i class="fas fa-concierge-bell"></i><span>Serviços</span></a></li>
+                <li><a href="user/login.php"><i class="fas fa-users"></i><span>Minha Conta</span></a></li>
+                <li><a href="#contact"><i class="fas fa-envelope"></i><span>Contato</span></a></li>
+                <?php
+                if (isset($_SESSION['id_login'])) {
+                ?>
+                    <li class="nav-item logout">
+                        <a href="#logout" class="nav-link"><i class="fas fa-sign-out-alt"></i><span>Desconectar</span></a>
+                    </li>
+                <?php
+                }
+                ?>
+            </ul>
+        </nav>
+       
+            
 
     <div class="container">
-    <div class="form-container">
-    <div class="form">
+    <div id="form-container-ctt" class="form-container">
+    <div id="form-ctt">
         <span class="heading">Contato</span>
         <input placeholder="Nome" type="text" class="input">
         <input placeholder="Email" id="mail" type="email" class="input">
