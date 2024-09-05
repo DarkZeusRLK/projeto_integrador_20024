97b675f37c02a86ce43fdd93f29e838501b52dcd
unknown <fernandesjoaopedro622@gmail.com>
2024-09-05 12:17:01 -0300

    att

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION)) {
 }
 
 $consultar_banco = "SELECT * FROM cadastro_hoteis";
-$retorno_consulta = $conexao->query($consultar_banco) or die($mysqli->error);
+$retorno_consulta = $conexao->query($consultar_banco) or die($conexao->error);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -30,15 +30,18 @@ $retorno_consulta = $conexao->query($consultar_banco) or die($mysqli->error);
             background-position-y: -40%;
             text-align: center;
         }
+
         .card {
             border: 1px solid #ddd;
             border-radius: 0.5rem;
             overflow: hidden;
         }
+
         .card img {
             width: 100%;
             height: auto;
         }
+
         .custom-btn {
             background-color: #007bff;
             color: white;
@@ -50,10 +53,12 @@ $retorno_consulta = $conexao->query($consultar_banco) or die($mysqli->error);
             display: inline-block;
             font-size: 1rem;
         }
+
         .custom-btn:hover {
             background-color: #0056b3;
             text-decoration: none;
         }
+
         .passagem {
             display: block;
             margin: 20px auto;
@@ -66,38 +71,38 @@ $retorno_consulta = $conexao->query($consultar_banco) or die($mysqli->error);
 </head>
 
 <body>
-<div id="carouselExampleDark" class="carousel carousel-dark slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="300">
-      <img class="img-fluid" src="Imagens/Banner para Site Mês dos Pais com Descontos Fotográfico Azul Escuro.png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-      </div>
+    <div id="carouselExampleDark" class="carousel carousel-dark slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="300">
+                <img class="img-fluid" src="Imagens/Banner para Site Mês dos Pais com Descontos Fotográfico Azul Escuro.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="300">
+                <img class="img-fluid" src="Imagens/Banner para Site Mês dos Pais com Descontos Fotográfico Azul Escuroo.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="300">
+                <img class="img-fluid" src="Imagens/Banner para Site Mês dos Pais com Descontos Fotográfico Azul Escuro (1).png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Anterior</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Próximo</span>
+        </button>
     </div>
-    <div class="carousel-item" data-bs-interval="300">
-      <img class="img-fluid" src="Imagens/Banner para Site Mês dos Pais com Descontos Fotográfico Azul Escuroo.png" class="d-block w-100" alt="..." >
-      <div class="carousel-caption d-none d-md-block">
-      </div>
-    </div>
-    <div class="carousel-item" data-bs-interval="300">
-      <img class="img-fluid" src="Imagens/Banner para Site Mês dos Pais com Descontos Fotográfico Azul Escuro (1).png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Anterior</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Próximo</span>
-  </button>
-</div>
     <div class="container-fluid">
         <nav class="col-md-3 col-lg-2 sidebar">
             <div class="menu-btn" onclick="toggleSidebar()">&#9776;</div>
@@ -108,28 +113,28 @@ $retorno_consulta = $conexao->query($consultar_banco) or die($mysqli->error);
             <ul class="nav-links">
                 <li><a href="#home"><i class="fas fa-home"></i><span>Home</span></a></li>
                 <li><a href="#services"><i class="fas fa-concierge-bell"></i><span>Serviços</span></a></li>
-                <?php if (isset($_SESSION['nome'])) : ?>
+                <?php if (isset($_SESSION['nome'])): ?>
                     <li><a href="user/minha_conta.php"><i class="fas fa-users"></i><span>Minha Conta</span></a></li>
-            <?php endif; ?>
-            <?php if (!isset($_SESSION['nome'])) : ?>
+                <?php endif; ?>
+                <?php if (!isset($_SESSION['nome'])): ?>
                     <li><a href="user/login.php"><i class="fas fa-users"></i><span>Minha Conta</span></a></li>
-            <?php endif; ?>
+                <?php endif; ?>
                 <li><a href="#contact"><i class="fas fa-envelope"></i><span>Contato</span></a></li>
                 <?php
                 if (isset($_SESSION['nome']) && $_SESSION["tipo_usuario"] === 'administrador') {
-                ?>
-                  <li><a href="admin/admin_dashboard.php"><i class="fas fa-tablet-alt"></i><span>Painel Adm</span></a></li>
+                    ?>
+                    <li><a href="admin/admin_dashboard.php"><i class="fas fa-tablet-alt"></i><span>Painel Adm</span></a></li>
 
-                <?php
+                    <?php
                 }
                 ?>
                 <?php
                 if (isset($_SESSION['nome'])) {
-                ?>
+                    ?>
                     <li class="nav-item logout">
                         <a href="static/logout.php" class="nav-link"><i class="fas fa-sign-out-alt"></i><span>Desconectar</span></a>
                     </li>
-                <?php
+                    <?php
                 }
                 ?>
             </ul>
@@ -137,10 +142,10 @@ $retorno_consulta = $conexao->query($consultar_banco) or die($mysqli->error);
         <main class="col-md-10 col-lg-10     main-content">
 
             <a class="passagem" href="passagem.php">Reserve sua Passagem</a>
-    
+
             <div class="row mt-4">
-                 <h1 id="text-index2">Hotéis em Destaque</h1>
-                <?php while ($hoteis = $retorno_consulta->fetch_assoc()) : ?>
+                <h1 id="text-index2">Hotéis em Destaque</h1>
+                <?php while ($hoteis = $retorno_consulta->fetch_assoc()): ?>
                     <div id="cards" class="col-md-3 mb-2">
                         <div class="card h-100">
                             <img src="<?php echo $hoteis['arquivo_caminho']; ?>" class="card-img-top" alt="...">
@@ -156,12 +161,12 @@ $retorno_consulta = $conexao->query($consultar_banco) or die($mysqli->error);
                     </div>
                 <?php endwhile; ?>
             </div>
-            
+
         </main>
     </div>
     <?php
-                    include('static/footer.php');
-                ?>
+    include('static/footer.php');
+    ?>
 </body>
 
 </html>
