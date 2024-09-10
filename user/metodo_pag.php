188baacf1d0a810256d9dfce28afb27c0e842e29
ZERE029 <matheusvital029@gmail.com>
2024-09-10 17:51:05 -0300

    att

--- user/metodo_pag.php
+++ user/metodo_pag.php
@@ -1,6 +1,6 @@
 <?php
 if (empty($opcaoSelecionada))
-    echo "Nenhuma opção selecionada.";
+    
 if (isset($_POST['metodo'])) {
     $metodo = $_POST['metodo'];
 
@@ -31,6 +31,7 @@ if (isset($_POST['metodo'])) {
     <title>Escolha Seu Método de Pagamento</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/zere.css">
+    <link rel="stylesheet" href="../css/style.css">
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -50,95 +51,101 @@ if (isset($_POST['metodo'])) {
         }
 
         .divo {
-        width: 100%;
-        padding: 30px;
-    }
+            width: 100%;
+            padding: 30px;
+        }
 
-    .profile-picture-container {
-        display: flex;
-        justify-content: center;
-    }
+        .profile-picture-container {
+            display: flex;
+            justify-content: center;
+        }
 
-    .profile-picture {
-        width: 150px;
-        height: 150px;
-        object-fit: cover;
-        border-radius: 50%;
-    }
+        .profile-picture {
+            width: 150px;
+            height: 150px;
+            object-fit: cover;
+            border-radius: 50%;
+        }
 
-    .button-group {
-        flex-direction: column;
-        align-items: center;
-        gap: 5px;
-    }
+        .button-group {
+            flex-direction: column;
+            align-items: center;
+            gap: 5px;
+        }
 
-    .form-control {
-        width: 100%;
-    }
+        .form-control {
+            width: 100%;
+        }
 
-    main {
-        padding: 10px;
-    }
+        main {
+            padding: 10px;
+        }
 
-    .info-title {
-        display: inline-block;
-        font-weight: bold;
-    }
+        .info-title {
+            display: inline-block;
+            font-weight: bold;
+        }
 
 
-/* Ajustes para telas maiores */
-@media (min-width: 769px) {
-    .profile-picture {
-        width: 200px;
-        height: 200px;
-    }
+        /* Ajustes para telas maiores */
+        @media (min-width: 769px) {
+            .profile-picture {
+                width: 200px;
+                height: 200px;
+            }
 
-    .button-group {
-        flex-direction: row;
-    }
+            .button-group {
+                flex-direction: row;
+            }
 
-    main {
-        padding: 20px;
-    }
-}
+            main {
+                padding: 20px;
+            }
+        }
     </style>
 </head>
 
 <body>
-    <h1>Escolha Seu Método de Pagamento</h1>
-    <div class="divo">
-        <form action="metodo_pag.php" method="post">
-            <div class="dive">
-                <div class="payment-option">
-                    <label>
-                        <input type="radio" name="metodo" value="cartao" required>
-                        <img src="../Imagens/credit-card.png" alt="" width="35px">
-                        Cartão de Crédito
-                    </label>
+    <div class="container">
+        <div id="form-container-ctt" class="form-container">
+            <form action="metodo_pag.php" method="post">
+                <div class="dive">
+                    <div class="payment-option">
+                        <label>
+                            <input type="radio" name="metodo" value="cartao" required>
+                            <img src="../Imagens/credit-card.png" alt="" width="35px">
+                            Cartão de Crédito
+                        </label>
+                    </div>
                 </div>
-            </div>
-            <div class="dive">
-            <div class="payment-option">
-                <label>
-                    <input type="radio" name="metodo" value="pix" required>
-                    <img src="../imagens/iconepix.png" alt="" width="25px">
-                    Pix
-                </label>
-            </div>
-            </div>
-    <div class="dive">
-    <div class="payment-option">
-                <label>
-                    <input type="radio" name="metodo" value="boleto" required>
-                    <img src="../imagens/boleto.png" alt="" height="20px">
-                    Transferência Bancária
-                </label>
-            </div>
-    </div>
-            <button type="submit" id="but1" class="btn btn-primary">Prosseguir</button>
-        </form>
+                <div class="dive">
+                    <div class="payment-option">
+                        <label>
+                            <input type="radio" name="metodo" value="pix" required>
+                            <img src="../imagens/iconepix.png" alt="" width="25px">
+                            Pix
+                        </label>
+                    </div>
+                </div>
+                <div class="dive">
+                    <div class="payment-option">
+                        <label>
+                            <input type="radio" name="metodo" value="boleto" required>
+                            <img src="../imagens/boleto.png" alt="" height="20px">
+                            Transferência Bancária
+                        </label>
+                    </div>
+                </div>
+                <div class="button-container">
+                        <div class="save-button-container">
+                            <button type="submit" class="save-button">Salvar</button>
+                        </div>
+                    </div>
+            </form>
+        </div>
     </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
 </html>
