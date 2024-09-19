f74b6270898bddb63e1c6dba3435f4483b3aa48f
JOAO RAFAEL DE CARVALHO BISSOLOTI <bissolotijoaorafael@hotmail.com>
2024-09-19 08:39:06 -0300

    Att

--- reservas.php
+++ reservas.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="css/reservas.css">
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
     <script defer src="javascript/script_navbar.js"></script>
     <title>Reserva de Passagens</title>
 </head>
+
 <body>
 
-<div class="container-fluid">
+    <div class="container-fluid">
         <nav class="col-md-3 col-lg-2 sidebar">
             <div class="menu-btn" onclick="toggleSidebar()">&#9776;</div>
             <div class="profile">
@@ -27,138 +32,125 @@
                 <?php
                 if (isset($_SESSION['id_login'])) {
                 ?>
-                    <li class="nav-item logout">
-                        <a href="#logout" class="nav-link"><i class="fas fa-sign-out-alt"></i><span>Desconectar</span></a>
-                    </li>
+                <li class="nav-item logout">
+                    <a href="#logout" class="nav-link"><i class="fas fa-sign-out-alt"></i><span>Desconectar</span></a>
+                </li>
                 <?php
                 }
                 ?>
             </ul>
         </nav>
 
-        
-        
-    
+
+
+
         <header>
-    <div class="filters">
-        <label class="city-filter" for="city-filter">Filtrar por cidade:</label>
-        <select id="city-filter">
-            <option value="todas">Todas</option>
-            <option value="ivaipora">Ivaiporã</option>
-            <option value="jardim-alegre">Jardim Alegre</option>
-        </select>
-    </div>
-</header>
+            <div class="filters">
+                <label class="city-filter" for="city-filter">Filtrar por cidade:</label>
+                <select id="city-filter">
+                    <option value="todas">Todas</option>
+                    <option value="ivaipora">Ivaiporã</option>
+                    <option value="jardim-alegre">Jardim Alegre</option>
+                </select>
+            </div>
+        </header>
 
 
-    <div class="espaco-reservas"></div>
+        <div class="espaco-reservas"></div>
 
-    <main>
-        <section class="card-container">
-        <div class="card">
-    <div class="image-container">
-        <img src="Imagens/senhor.jpg" alt="Imagem 2">
-        <a href="reservado.php">
-            <i class="fas fa-check-circle select-icon"></i>
-        </a>
-    </div>
-    <div class="card-info">
-        <h3>Nome do Hotel</h3>
-        <p>Cidade do Hotel</p>
-        <p>Valor do Hotel</p>
-    </div>
-</div>
+        <main>
+            <section class="card-container">
+                <a href="reservado.php" style="text-decoration: none;">
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="Imagens/senhor.jpg" alt="Imagem 2">
+                            <i class="fas fa-check-circle select-icon"></i>
+                        </div>
+                        <div class="card-info">
+                            <h3>Vilhar Palace Hotel</h3>
+                            <p>Ivaiporã</p>
+                            <p>A partir de R$ 200,00</p>
+                        </div>
+                    </div>
+                </a>
 
-            <div class="card">
-    <div class="image-container">
-        <img src="Imagens/senhor.jpg" alt="Imagem 2">
-        <a href="reservado.php">
-            <i class="fas fa-check-circle select-icon"></i>
-        </a>
-    </div>
-    <div class="card-info">
-        <h3>Nome do Hotel</h3>
-        <p>Cidade do Hotel</p>
-        <p>Valor do Hotel</p>
-        
-        
-    </div>
-</div>
+                <a href="reservado.php" style="text-decoration: none;">
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="Imagens/senhor.jpg" alt="Imagem 2">
+                            <i class="fas fa-check-circle select-icon"></i>
+                        </div>
+                        <div class="card-info">
+                            <h3>Nome do Hotel</h3>
+                            <p>Cidade do Hotel</p>
+                            <p>Valor do Hotel</p>
+                        </div>
+                    </div>
+                </a>
 
-<div class="card">
-    <div class="image-container">
-        <img src="Imagens/senhor.jpg" alt="Imagem 2">
-        <a href="reservado.php">
-            <i class="fas fa-check-circle select-icon"></i>
-        </a>
-    </div>
-    <div class="card-info">
-        <h3>Nome do Hotel</h3>
-        <p>Cidade do Hotel</p>
-        <p>Valor do Hotel</p>
-        
-        
-    </div>
-</div>
+                <a href="reservado.php" style="text-decoration: none;">
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="Imagens/senhor.jpg" alt="Imagem 2">
+                            <i class="fas fa-check-circle select-icon"></i>
+                        </div>
+                        <div class="card-info">
+                            <h3>Nome do Hotel</h3>
+                            <p>Cidade do Hotel</p>
+                            <p>Valor do Hotel</p>
+                        </div>
+                    </div>
+                </a>
 
+                <a href="reservado.php" style="text-decoration: none;">
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="Imagens/senhor.jpg" alt="Imagem 2">
+                            <i class="fas fa-check-circle select-icon"></i>
+                        </div>
+                        <div class="card-info">
+                            <h3>Nome do Hotel</h3>
+                            <p>Cidade do Hotel</p>
+                            <p>Valor do Hotel</p>
+                        </div>
+                    </div>
+                </a>
 
+                <a href="reservado.php" style="text-decoration: none;">
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="Imagens/senhor.jpg" alt="Imagem 2">
+                            <i class="fas fa-check-circle select-icon"></i>
+                        </div>
+                        <div class="card-info">
+                            <h3>Nome do Hotel</h3>
+                            <p>Cidade do Hotel</p>
+                            <p>Valor do Hotel</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="reservado.php" style="text-decoration: none;">
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="Imagens/senhor.jpg" alt="Imagem 2">
+                            <i class="fas fa-check-circle select-icon"></i>
+                        </div>
+                        <div class="card-info">
+                            <h3>Nome do Hotel</h3>
+                            <p>Cidade do Hotel</p>
+                            <p>Valor do Hotel</p>
+                        </div>
+                    </div>
+                </a>
+            </section>
+        </main>
 
-<div class="card">
-    <div class="image-container">
-        <img src="Imagens/senhor.jpg" alt="Imagem 2">
-        <a href="reservado.php">
-            <i class="fas fa-check-circle select-icon"></i>
-        </a>
-    </div>
-    <div class="card-info">
-        <h3>Nome do Hotel</h3>
-        <p>Cidade do Hotel</p>
-        <p>Valor do Hotel</p>
-        
-        
-    </div>
-</div>
-
-<div class="card">
-    <div class="image-container">
-        <img src="Imagens/senhor.jpg" alt="Imagem 2">
-        <a href="reservado.php">
-            <i class="fas fa-check-circle select-icon"></i>
-        </a>
-    </div>
-    <div class="card-info">
-        <h3>Nome do Hotel</h3>
-        <p>Cidade do Hotel</p>
-        <p>Valor do Hotel</p>
-        
-        
-    </div>
-</div>
-
-<div class="card">
-    <div class="image-container">
-        <img src="Imagens/senhor.jpg" alt="Imagem 2">
-        <a href="reservado.php">
-            <i class="fas fa-check-circle select-icon"></i>
-        </a>
-    </div>
-    <div class="card-info">
-        <h3>Nome do Hotel</h3>
-        <p>Cidade do Hotel</p>
-        <p>Valor do Hotel</p>
-        
-        
-    </div>
-</div>
-        </section>
-    </main>
-
-    <div vw class="enabled">
-        <div vw-access-button class="active"></div>
-        <div vw-plugin-wrapper>
-            <div class="vw-plugin-top-wrapper"></div>
+        <div vw class="enabled">
+            <div vw-access-button class="active"></div>
+            <div vw-plugin-wrapper>
+                <div class="vw-plugin-top-wrapper"></div>
+            </div>
         </div>
-    </div>
     </div>
     <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
     <script>
@@ -167,15 +159,16 @@
 
 
 
-    
 
 
 
 
-        <?php
+
+    <?php
             include("static/footer.php");
         ?>
-    
+
 
 </body>
+
 </html>
