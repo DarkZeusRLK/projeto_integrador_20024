909a8026f3373aaf2c0b066ed37bde959e30489b
ZERE029 <matheusvital029@gmail.com>
2024-09-30 16:52:59 -0300

    att

--- metodo_pag.php
+++ metodo_pag.php
@@ -12,6 +12,37 @@
 
 <body>
 
+<div class="container-fluid">
+        <nav class="col-md-3 col-lg-2 sidebar">
+            <div class="menu-btn" onclick="toggleSidebar()">&#9776;</div>
+            <div class="profile">
+                <img id="logo" src="../Imagens/logo (1).png" alt="Logo">
+                <h1 class="text-title">IvaíTour</h1>
+            </div>
+            <ul class="nav-links">
+                <li><a href="../index.php"><i class="fas fa-home"></i><span>Home</span></a></li>
+                <li><a href="#services"><i class="fas fa-concierge-bell"></i><span>Serviços</span></a></li>
+                <?php if (isset($_SESSION['nome'])): ?>
+                    <li><a href="conta.php"><i class="fas fa-users"></i><span>Minha Conta</span></a></li>
+                <?php else: ?>
+                    <li><a href="login.php"><i class="fas fa-users"></i><span>Minha Conta</span></a></li>
+                <?php endif; ?>
+                <li><a href="#contact"><i class="fas fa-envelope"></i><span>Contato</span></a></li>
+                <?php if (isset($_SESSION['nome']) && $_SESSION["tipo_usuario"] === 'administrador'): ?>
+                    <li><a href="../admin/admin_dashboard.php"><i class="fas fa-tablet-alt"></i><span>Painel Adm</span></a></li>
+                <?php endif; ?>
+                <?php if (isset($_SESSION['nome'])): ?>
+                    <li class="nav-item logout">
+                        <a href="../static/logout.php" class="nav-link"><i class="fas fa-sign-out-alt"></i><span>Desconectar</span></a>
+                    </li>
+                <?php endif; ?>
+                <li class="nav-item">
+                    <a href="configuracoes.php" class="nav-link" id="settings-icon">
+                        <i class="fas fa-cog"></i><span>Configurações</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
 
 
 
