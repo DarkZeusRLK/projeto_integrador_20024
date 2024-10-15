795c69661ddc99740ffe749328eebcfe85dfa2fb
ZERE029 <matheusvital029@gmail.com>
2024-10-15 19:34:54 -0300

    att

--- page/contato.php
+++ page/contato.php
@@ -59,6 +59,7 @@ $foto = isset($_SESSION['arquivo_foto']) ? $_SESSION['arquivo_foto'] : 'caminho/
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../estilo.css">
     <script defer src="../javascript/script_navbar.js"></script>
     <script defer src="../javascript/alternar_modos.js"></script>
     <script defer src="../javascript/cookie.js"></script>
@@ -175,7 +176,7 @@ $foto = isset($_SESSION['arquivo_foto']) ? $_SESSION['arquivo_foto'] : 'caminho/
         </script>
     </div>
     
-    <?php include('../static/footer.php');
+    <?php include('../rod.php');
     
     ?>
 </body>
