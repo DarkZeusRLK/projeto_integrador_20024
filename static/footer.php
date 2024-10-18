053ae5305ee99ced7c9b30f3158bfa90f2862433
Dieimes Nunes <114628166+DieimesNunes@users.noreply.github.com>
2024-10-18 10:28:30 -0300

    add

--- static/footer.php
+++ static/footer.php
@@ -1,77 +1,74 @@
-<div class="borda"> <main>
 
-</main>
-    <footer>
-        <div id="footer_content">
-            <div id="footer_contacts">
-                <a href="../index/index.php"><img src="../Imagens/logo (1).png" alt="" width="100rem" height="90rem"></a>
-                <p>Agência de Viagens e Turismo</p>
+<footer>
+    <div id="footer_content">
+        <div id="footer_contacts">
+            <a href="../index/index.php"><img src="../Imagens/logo (1).png" alt="" width="100rem"
+                    height="90rem"></a>
+            <p>Agência de Viagens e Turismo</p>
 
-                <div id="footer_social_media">
-                    <a href="#" class="footer-link" id="instagram">
-                        <i class="fa-brands fa-instagram"></i>
-                    </a>
+            <div id="footer_social_media">
+                <a href="#" class="footer-link" id="instagram">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
 
-                    <a href="#" class="footer-link" id="facebook">
-                        <i class="fa-brands fa-facebook-f"></i>
-                    </a>
+                <a href="#" class="footer-link" id="facebook">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </a>
 
-                    <a href="#" class="footer-link" id="whatsapp">
-                        <i class="fa-brands fa-whatsapp"></i>
-                    </a>
-                </div>
-            </div>
-            
-            <ul class="footer-list">
-                <li>
-                    <h3>Blog</h3>
-                </li>
-                <li>
-                    <a href="https://blog.runrun.it/avanco-da-tecnologia/" class="footer-link">Tecnologia</a>
-                </li>
-                <li>
-                    <a href="https://www.almadeviajante.com/viagens/brasil/" class="footer-link">Turismo no Brasil</a>
-                </li>
-                <li>
-                    <a href="https://blog.betrybe.com/desenvolvimento-web/" class="footer-link">Desenvolvimento Web</a>
-                </li>
-            </ul>
-
-            <ul class="footer-list">
-                <li>
-                    <h3>Serviços</h3>
-                </li>
-                <li>
-                    <a href="../index/index.php" class="footer-link">Hotéis</a>
-                </li>
-                <li>
-                    <a href="../user/login.php" class="footer-link">Login</a>
-                </li>
-                <li>
-                    <a href="../index/index.php" class="footer-link">Pacotes de Viagens</a>
-                </li>
-            </ul>
-
-            <div id="footer_subscribe">
-                <h3>Contate-nos</h3>
-
-                <p>
-                   Envie uma mensagem
-                </p>
-
-                <div id="input_group">
-                    <input type="email" id="email">
-                    <button>
-                        <i id="envelope" class="fa-regular fa-envelope"></i>
-                    </button>
-                </div>
+                <a href="#" class="footer-link" id="whatsapp">
+                    <i class="fa-brands fa-whatsapp"></i>
+                </a>
             </div>
         </div>
 
-        <div id="footer_copyright">
-        IvaíTour &#169
-            2024 Todos os Direitos Reservados
+        <ul class="footer-list">
+            <li>
+                <h3>Blog</h3>
+            </li>
+            <li>
+                <a href="https://blog.runrun.it/avanco-da-tecnologia/" class="footer-link">Tecnologia</a>
+            </li>
+            <li>
+                <a href="https://www.almadeviajante.com/viagens/brasil/" class="footer-link">Turismo no Brasil</a>
+            </li>
+            <li>
+                <a href="https://blog.betrybe.com/desenvolvimento-web/" class="footer-link">Desenvolvimento Web</a>
+            </li>
+        </ul>
+
+        <ul class="footer-list">
+            <li>
+                <h3>Serviços</h3>
+            </li>
+            <li>
+                <a href="../index/index.php" class="footer-link">Hotéis</a>
+            </li>
+            <li>
+                <a href="../user/login.php" class="footer-link">Login</a>
+            </li>
+            <li>
+                <a href="../index/index.php" class="footer-link">Pacotes de Viagens</a>
+            </li>
+        </ul>
+
+        <div id="footer_subscribe">
+            <h3>Contate-nos</h3>
+
+            <p>
+                Envie uma mensagem
+            </p>
+
+            <div id="input_group">
+                <input type="email" id="email">
+                <button>
+                    <i id="envelope" class="fa-regular fa-envelope"></i>
+                </button>
+            </div>
         </div>
-    </footer>
     </div>
-   
+
+    <div id="footer_copyright">
+        IvaíTour &#169
+        2024 Todos os Direitos Reservados
+    </div>
+</footer>
