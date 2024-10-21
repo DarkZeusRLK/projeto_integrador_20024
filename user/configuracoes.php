7f386b915bcf4dc2c4e2c150540f323758397e2c
MATHEUS HENRIQUE VITAL <vital.2907@aluno.pr.senac.br>
2024-10-21 08:51:40 -0300

    att

--- user/configuracoes.php
+++ user/configuracoes.php
@@ -55,73 +55,74 @@ A<?php
       if (isset($_SESSION['nome'])) {
 
       ?>
-        <div class="user-profile">
-          <span class="username"><b><?php echo $_SESSION['nome']; ?></b></span>
-          <?php if ($tipo_usuario === 'administrador'): ?>
-            <span id="admin-badge">ADM</span>
-          <?php endif; ?>
-          <a href="conta.php" class="user-avatar-link">
-            <img src="<?php echo $foto; ?>" alt="Avatar" class="avatar">
-          </a> <!-- Faltava o fechamento da tag <a> -->
-        </div> <!-- Faltava o fechamento da tag <div> -->
+    <div class="user-profile">
+  <span class="username"><b><?php echo $_SESSION['nome']; ?></b></span>
+  <?php if ($tipo_usuario === 'administrador'): ?>
+    <span id="admin-badge">ADM</span>
+  <?php endif; ?>
+  <a href="conta.php" class="user-avatar-link">
+    <img src="<?php echo $foto; ?>" alt="Avatar" class="avatar">
+  </a> <!-- Faltava o fechamento da tag <a> -->
+</div> <!-- Faltava o fechamento da tag <div> -->
 
       <?php
       }
       ?>
 
       <!-- Alternar modo dia/noite -->
-      <div class="mt-4">
-        <h3>Modo de Exibição</h3>
-        <div class="theme-toggle-container">
-          <span id="day-icon" class="fas fa-sun"></span>
-          <label class="switch">
-            <input type="checkbox" id="theme-toggle">
-            <span class="slider round"></span>
-          </label>
-          <span id="night-icon" class="fas fa-moon"></span>
-        </div>
-      </div>
+       <div class="container-fluid">
 
-      <!-- Alterar imagem de fundo do index -->
-      <div class="container-fluid2">
-        <h3>Alterar Imagem de Fundo</h3>
-        <form id="background-form">
-          <div class="form-group w-auto">
-            <label for="background-select">Selecione a Imagem/GIF de Fundo:</label>
-            <select class="form-control" id="background-select">
-              <option value="../Imagens/Vegetação2.gif">Avião GIF</option>
-              <option value="../Imagens/a733d129bf370f5085507c89b6f3272c.gif">Praia GIF</option>
-              <option value="../Imagens/pngtree-8-best-free-mountain-background-images-4k-wallpapers-image_2670051.jpg">Montanha</option>
-              <option value="../Imagens/penhasco.gif">Penhasco GIF</option>
-            </select>
-          </div>
-
-
-
-          <br>
-          <button type="submit" class="custom-btn">Salvar Alterações</button>
-        </form>
-
-      <!-- Alterar tamanho da fonte -->
-        <h3>Tamanho da Fonte</h3>
-        <div class="form-group">
-          <label for="font-size-select">Selecione o Tamanho da Fonte:</label>
-          <select class="form-control" id="font-size-select">
-            <option value="16px">Padrão (16px)</option>
-            <option value="18px">Médio (18px)</option>
-            <option value="20px">Grande (20px)</option>
-            <option value="22px">Extra Grande (22px)</option>
-          </select>
-        </div>
-        <br>
-        <button id="save-font-size" class="custom-btn">Salvar Tamanho da Fonte</button>
-      </div>
-    </div>
-    </div>
-  </div>
-  <?php
-  include('../static/footer.php');
-  ?>
+         <div class="mt-4">
+           <h3>Modo de Exibição</h3>
+           <div class="theme-toggle-container">
+             <span id="day-icon" class="fas fa-sun"></span>
+             <label class="switch">
+               <input type="checkbox" id="theme-toggle">
+               <span class="slider round"></span>
+             </label>
+             <span id="night-icon" class="fas fa-moon"></span>
+           </div>
+         </div>
+   
+         <!-- Alterar imagem de fundo do index -->
+         <div class="mt-4">
+           <h3>Alterar Imagem de Fundo</h3>
+           <form id="background-form">
+             <div class="form-group">
+               <label for="background-select">Selecione a Imagem/GIF de Fundo:</label>
+               <select class="form-control" id="background-select">
+                 <option value="Imagens/Vegetação2.gif">Avião GIF</option>
+                 <option value="Imagens/a733d129bf370f5085507c89b6f3272c.gif">Praia GIF</option>
+                 <option value="Imagens/pngtree-8-best-free-mountain-background-images-4k-wallpapers-image_2670051.jpg">Montanha</option>
+                 <option value="Imagens/penhasco.gif">Penhasco GIF</option>
+               </select>
+             </div>
+             <br>
+             <button type="submit" class="custom-btn">Salvar Alterações</button>
+           </form>
+         </div>
+   
+         <!-- Alterar tamanho da fonte -->
+         <div class="mt-4">
+           <h3>Tamanho da Fonte</h3>
+           <div class="form-group">
+             <label for="font-size-select">Selecione o Tamanho da Fonte:</label>
+             <select class="form-control" id="font-size-select">
+               <option value="16px">Padrão (16px)</option>
+               <option value="18px">Médio (18px)</option>
+               <option value="20px">Grande (20px)</option>
+               <option value="22px">Extra Grande (22px)</option>
+             </select>
+           </div>
+           <br>
+           <button id="save-font-size" class="custom-btn">Salvar Tamanho da Fonte</button>
+         </div>
+       </div>
+     </div>
+       </div>
+       <?php 
+       include('../static/footer.php');
+       ?>
 </body>
 
 </html>
