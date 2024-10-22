fdf9652cac2903c0b94bc10b626089ec56f27142
ZERE029 <matheusvital029@gmail.com>
2024-10-22 16:34:36 -0300

    att

--- static/footer.php
+++ static/footer.php
@@ -1,5 +1,76 @@
-<div class="footer text-center"> 
-        <p><b>&copy; 2024 IVAÍTOUR. Todos os direitos reservados.</b></p>
-        <p>Orgulhosamente Desenvolvido por <a href="https://gist.github.com/DarkZeusRLK"> João Pedro Fernandes</a>, <a href="https://gist.github.com/jraffis">João Rafael</a>,<a href="https://gist.github.com/joaquim22br"> Joaquim Sagioneti</a>, <a href="https://gist.github.com/luizeduardo2022">Luiz Eduardo</a> e <a href="https://gist.github.com/zere029">Matheus Vital</a></p>
-        <p>Orientado por <a href="https://gist.github.com/dieimesnunes">Dieimes Nunes de Souza</a></p>
-</div>
+<footer>
+    <div id="footer_content">
+        <div id="footer_contacts">
+            <a href="../index/index.php"><img src="../Imagens/logo (1).png" alt="" width="100rem" height="90rem"></a>
+            <p>Agência de Viagens e Turismo</p>
+
+            <div id="footer_social_media">
+                <a href="#" class="footer-link" id="instagram">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+
+                <a href="#" class="footer-link" id="facebook">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </a>
+
+                <a href="#" class="footer-link" id="whatsapp">
+                    <i class="fa-brands fa-whatsapp"></i>
+                </a>
+            </div>
+        </div>
+
+        <ul class="footer-list">
+            <li>
+                <h3><a class="footer-link" href="../index/index.php#blogs">Blogs</a></h3>
+            </li>
+            <li>
+                <a href="https://blog.runrun.it/avanco-da-tecnologia/" class="footer-link">Tecnologia</a>
+            </li>
+            <li>
+                <a href="https://www.almadeviajante.com/viagens/brasil/" class="footer-link">Turismo no Brasil</a>
+            </li>
+            <li>
+                <a href="https://blog.betrybe.com/desenvolvimento-web/" class="footer-link">Desenvolvimento Web</a>
+            </li>
+        </ul>
+
+        <ul class="footer-list">
+            <li>
+                <h3>Serviços</h3>
+            </li>
+            <li>
+                <a href="../index/index.php#hoteis" class="footer-link">Hotéis</a>
+            </li>
+            <li>
+                <a href="../user/login.php" class="footer-link">Login</a>
+            </li>
+            <li>
+                <a href="../index/index.php#card_pacote" class="footer-link">Pacotes de Viagens</a>
+            </li>
+        </ul>
+
+        <div id="footer_subscribe">
+            <form action="" method="POST">
+                <h3>Contate-nos</h3>
+
+                <p>
+                    Envie uma mensagem
+                </p>
+
+                <div id="input_group">
+                    <input name="email" type="email" id="email" placeholder="Seu Email">
+                    </div>
+                    <br>
+                    <div id="input_group">
+                    <input name="nome" type="text" id="email" placeholder="Seu Nome">
+                    </div>
+                    <br>
+                    <div id="input_group">
+                    <input name="mensagem" type="text" id="email" placeholder="Sua Mensagem">
+                    <button type="submit">
+                        <i id="envelope" class="fa-regular fa-envelope"></i>
+                        </button>
+                    </div>
+            </form>
+        </div>
+</footer>
