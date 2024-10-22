3fc9c3a80f017d7b482bd5ff71f5977e8f789e5c
ZERE029 <matheusvital029@gmail.com>
2024-10-22 17:06:47 -0300

    att

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -20,8 +20,8 @@ $retorno_consulta2 = $conexao->query($consultar_banco2) or die($conexao->error);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../css/style.css">
-    
-    
+
+
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script defer src="../javascript/dashboard_adm.js"></script>
     <script defer src="../javascript/script_navbar.js"></script>
@@ -32,93 +32,77 @@ $retorno_consulta2 = $conexao->query($consultar_banco2) or die($conexao->error);
 </head>
 <style>
     /* Definições gerais para manter a tabela responsiva */
-.my-custom-table {
-    width: 100%;
-    overflow-x: auto;
-    display: block;
-    margin-bottom: 20px;
-}
-
-.table-container {
-    width: 100%;
-    overflow-x: auto;
-}
-
-table {
-    width: 100%;
-    border-collapse: collapse;
-    text-align: left;
-}
-
-table th, table td {
-    padding: 10px;
-    border: 1px solid #ddd;
-}
-
-/* Ajustes de layout em diferentes larguras de tela */
-@media (max-width: 768px) {
-.footer{
-    margin-top: 40vh;
-}
-    table th, table td {
-        font-size: 14px;
-        padding: 2px;
-    }
-
-    img#imagem {
-        width: 80px;
-        height: 80px;
-    }
-
-    .imagem-dashboard-adm {
-        width: 100px;
-        height: auto;
-    }
-
-    .custom-btn2 {
+    .my-custom-table {
         width: 100%;
-        margin-bottom: 10px;
-    }
-}
-
-@media (max-width: 576px) {
-    .footer{
-        margin-top: 45vh;
-    }
-    table th, table td {
-        font-size: 12px;
-        padding: 5px;
-        color: #ddd;
-    }
-.dashboard-content{
-display: contents;
-}
-    img#imagem {
-        width: 60px;
-        height: 60px;
+        overflow-x: auto;
+        display: block;
+        margin-bottom: 20px;
     }
 
-    .imagem-dashboard-adm {
-        width: 8px;
-    }
-
-    /* Exibição das tabelas em blocos verticais no celular */
-    .my-custom-table, .table-container {
-        overflow-x: scroll;
-    }
-
-    .custom-btn2 {
+    .table-container {
         width: 100%;
+        overflow-x: auto;
     }
-}
 
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        text-align: left;
+    }
+
+    table th,
+    table td {
+        padding: 10px;
+        border: 1px solid #ddd;
+    }
+
+    /* Ajustes de layout em diferentes larguras de tela */
+    @media (max-width: 768px) {
+       
+
+        table td {
+            font-size: 13px;
+            padding: 10px;
+        }
+        .dashboard-content {
+            width: 70%;
+            margin-left: 5%;
+        }
+        .custom-btn2 {
+            width: 100%;
+            margin-bottom: 10px;
+        }
+    }
+
+    @media (max-width: 576px) {
+        table td {
+            font-size: 11px;
+            padding: 10px;
+            color: #000;
+        }
+
+        .dashboard-content {
+            width: 70%;
+            margin-left: 14%;
+        }
+
+
+        /* Exibição das tabelas em blocos verticais no celular */
+        .my-custom-table,
+        .table-container {
+            overflow-x: scroll;
+        }
+
+        .custom-btn2 {
+            width: 100%;
+        }
+    }
 </style>
-<body>
 
-    <div class="container-fluid">
-    <?php
+<body>
+        <?php
         include('../static/menu.php');
-       ?>
+        ?>
         <main class="col-md-10 col-lg-10 main-content">
             <section class="dashboard-content">
                 <div class="options">
@@ -139,15 +123,16 @@ display: contents;
                             </tr>
                         </thead>
                         <tbody>
-                        <?php while ($user = $retorno_consulta->fetch_assoc()) : ?>
-        <tr id="link_adm_table" onclick="window.location.href='alterar_ou_deletar.php?codigo_cadastro=<?php echo $user['id_usuario'];?>'" style="cursor: pointer;">
-            <td><?php echo $user['id_usuario']; ?></td>
-            <td><?php echo $user['nome']; ?></td>
-            <td><?php echo $user['email']; ?></td>
-            <td><?php echo $user['tipo_usuario']; ?> 
-            <td><i class="fas fa-cog"> </i><span> Editar</span></td>   </td>
-        </tr>
-    <?php endwhile; ?>
+                            <?php while ($user = $retorno_consulta->fetch_assoc()) : ?>
+                                <tr id="link_adm_table" onclick="window.location.href='alterar_ou_deletar.php?codigo_cadastro=<?php echo $user['id_usuario']; ?>'" style="cursor: pointer;">
+                                    <td><?php echo $user['id_usuario']; ?></td>
+                                    <td><?php echo $user['nome']; ?></td>
+                                    <td><?php echo $user['email']; ?></td>
+                                    <td><?php echo $user['tipo_usuario']; ?>
+                                    <td><i class="fas fa-cog"> </i><span> Editar</span></td>
+                                    </td>
+                                </tr>
+                            <?php endwhile; ?>
                         </tbody>
                     </table>
                 </div>
@@ -166,28 +151,28 @@ display: contents;
                             </tr>
                         </thead>
                         <tbody>
-                        <?php while ($hotel = $retorno_consulta2->fetch_assoc()) : ?>
-                            <tr id="link_adm_table" onclick="window.location.href='alterar_ou_deletar.php?codigo_cadastro=<?php echo $hotel['id_hotel'];?>'" style="cursor: pointer;">
-                                <td><?php echo $hotel['id_hotel'];?></td>
-                                <td><?php echo $hotel['nome'];?></td>
-                                <td><?php echo $hotel['descricao'];?></td>
-                                <td><img src="../<?php echo $hotel['arquivo_caminho'];?>" alt="Imagem do hotel" class="imagem-dashboard-adm"></td>
-                                <td>Hotel</td>
-                                <td><i class="fas fa-cog"> </i><span> Editar</span></td>   </td>
+                            <?php while ($hotel = $retorno_consulta2->fetch_assoc()) : ?>
+                                <tr id="link_adm_table" onclick="window.location.href='alterar_ou_deletar.php?codigo_cadastro=<?php echo $hotel['id_hotel']; ?>'" style="cursor: pointer;">
+                                    <td><?php echo $hotel['id_hotel']; ?></td>
+                                    <td><?php echo $hotel['nome']; ?></td>
+                                    <td><?php echo $hotel['descricao']; ?></td>
+                                    <td><img src="../<?php echo $hotel['arquivo_caminho']; ?>" alt="Imagem do hotel" class="imagem-dashboard-adm"></td>
+                                    <td>Hotel</td>
+                                    <td><i class="fas fa-cog"> </i><span> Editar</span></td>
+                                    </td>
 
-                                </td>
-                            </tr>
+                                    </td>
+                                </tr>
                             <?php endwhile; ?>
 
                         </tbody>
                     </table>
-                  <a class="custom-btn2" href="../user/cadastrar_hoteis_gramado.php">Cadastrar Hotéis Gramado</a>
+                    <a class="custom-btn2" href="../user/cadastrar_hoteis_gramado.php">Cadastrar Hotéis Gramado</a>
                 </div>
             </section>
         </main>
-    </div>
     <?php
-        include('../static/footer.php');
+    include('../static/footer.php');
     ?>
 
     <script src="../javascript/script.js"></script>
