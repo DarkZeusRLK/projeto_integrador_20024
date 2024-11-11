8ee3256fc464de320d4e12694bc0a93f6a8534ad
ZERE029 <matheusvital029@gmail.com>
2024-11-11 15:18:54 -0300

    att

--- user/login.php
+++ user/login.php
@@ -56,6 +56,7 @@ $conexao->close();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -83,17 +84,16 @@ $conexao->close();
             font-size: 14px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
         }
-    </style>    
+    </style>
 </head>
 
 <body>
-<div class="container">
     
-</div>
-    <div class="container-fluid2">
+
         <?php
         include('../static/menu.php');
         ?>
+    <div class="container-fluid2">
         <form class="form" method="POST">
             <div class="flex-column">
                 <label>Email </label>
@@ -165,22 +165,18 @@ $conexao->close();
                 </button>
             </div>
         </form>
-        <br>
-        <br>
     </div>
-    <?php
-    include("../static/footer.php");
-    ?>
-      <?php if ($mensagem): ?>
-            <div id="alerta-erro" class="alerta-erro">
-                <span><?php echo $mensagem; ?></span>
-            </div>
-            <script>
-                setTimeout(() => {
-                    document.getElementById("alerta-erro").style.display = "none";
-                }, 5000);
-            </script>
-        <?php endif; ?>
+    <?php include("../static/footer.php");?>
+    <?php if ($mensagem): ?>
+        <div id="alerta-erro" class="alerta-erro">
+            <span><?php echo $mensagem; ?></span>
+        </div>
+        <script>
+            setTimeout(() => {
+                document.getElementById("alerta-erro").style.display = "none";
+            }, 5000);
+        </script>
+    <?php endif; ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
